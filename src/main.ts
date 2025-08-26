0e3de557cdff385d57f5bda46e88903668dc3944
Asif <2430012@cognizant.com>
2025-08-26 18:20:36 +0530

    Add base structure

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { Env } from './env/env';
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
+
+// Dynamic AppName
+document.title = Env.appName;
+
+// Dynamic Favicon
+const favicon = document.querySelector("link[rel~='icon']") as HTMLLinkElement;
+favicon.href = Env.appLogo;
+
+
