932bfdd23c487fa2eef27ffc6fd552b5cff7f4c4
Asif <2430012@cognizant.com>
2025-08-28 17:52:18 +0530

    Add categoryList to home

--- src/app/components/pages/home/home.ts
+++ src/app/components/pages/home/home.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HomeService } from '../../../services/home-service/home-service';
+import { CommonModule } from '@angular/common';
+import { Env } from '../../../../env/env';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
-export class Home {
+export class Home implements OnInit{
+  appName = Env.appName;
 
+  categoryList: any[] = [];
+
+  constructor(private homeService: HomeService) {}
+  
+  ngOnInit(): void {
+   this.homeService.getCategoryList().subscribe(data => {this.categoryList = data});
+  }
 }
