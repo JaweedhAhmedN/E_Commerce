b23c56b222954128306dda6d0bc38a7a70227618
Asif <2430012@cognizant.com>
2025-08-29 16:13:57 +0530

    Add term-conditions

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { NotFound } from './components/pages/not-found/not-found';
 import { Home } from './components/pages/home/home';
 import { AboutUs } from './components/common/footer/help-support/about-us/about-us';
 import { Faq } from './components/common/footer/help-support/faq/faq';
+import { TermsConditions } from './components/common/footer/help-support/terms-conditions/terms-conditions';
 
 export const routes: Routes = [
     /* App Routes */
     // Footer Help-Support Routes
     {path: 'about-us', component: AboutUs},
     {path: 'faq', component: Faq},
+    {path: 'terms-conditions', component: TermsConditions},
 
     // Global Routes
     {path: '', component: Home},
